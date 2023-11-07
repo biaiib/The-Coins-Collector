58bfb1e7077d8532c10b067f7770601fcdfbf268
Beatriz <beatriz2005ss@gmail.com>
2023-11-07 19:25:15 -0300

    Fixed bug
    The coins counter was not reseting when a new game was started. That was fixed.

--- TheCC/Assets/Scripts/Coletaveis/ContadorMoedas.cs
+++ TheCC/Assets/Scripts/Coletaveis/ContadorMoedas.cs
@@ -9,6 +9,11 @@ public class ContadorMoedas: MonoBehaviour
     public static int contadorMoedas;
     public GameObject displayContadorMoedas;
     public GameObject MoedasFinal;
+
+    void Start()
+    {
+        contadorMoedas = 0; //inicializa o contador para zero ao iniciar o jogo.
+    }
     void Update()
     {
         displayContadorMoedas.GetComponent<TextMeshProUGUI>().text = "" + contadorMoedas;
